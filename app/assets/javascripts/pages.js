1909a8fb5c39b4b2db4f806509b114c503ecae59
Wojciech Bućko <wojciech.bucko@netguru.co>
2016-07-22 19:33:40 +0200

    Added slider to home page

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,46 @@
+$(document).ready(function($element){
+  var speed = 500;
+  var autoswitch = false;
+  var autoswitch_speed = 4000;
+  var $sliderElement = $element.find('.slider-element');
+  var $active = $element.find('.active');
+  var $oldActive = $element.find('.oldActive');
+  var $next = $element.find('.next');
+  var $prev = $element.find('.prev');
+
+
+  $($sliderElement).first().addClass('active');
+  $($sliderElement).hide();
+  $('.active').show();
+
+  $($next).click(nextSlide);
+  $($prev).click(prevSlide);
+
+  if (autoswitch == true) {
+    setInterval(nextSlide, autoswitch_speed);
+  };
+
+  function nextSlide() {
+    $('.active').removeClass('active').addClass('oldActive');
+    if ($('.oldActive').is(':last-child')){
+      $($sliderElement).first().addClass('active');
+    } else {
+      $('.oldActive').next().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $($sliderElement).fadeOut(speed);
+    $('.active').fadeIn(speed);
+  };
+
+  function prevSlide() {
+    $('.active').removeClass('active').addClass('oldActive');
+    if ($('.oldActive').is(':first-child')){
+      $($sliderElement).last().addClass('active');
+    } else {
+      $('.oldActive').prev().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $($sliderElement).fadeOut(speed);
+    $('.active').fadeIn(speed);
+  };
+});
